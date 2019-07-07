af72d7f996d039b44cf7d7e6d4a95bd8ddd829ab
ꌗᖘ꒒ꀤ꓄꒒ꀤꈤꍟ <tbsthitw@gmail.com>
2019-07-07 18:28:08 +0800

    Create auto-fill-final-evaluation.js

---
+++ auto-fill-final-evaluation.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('font>a').forEach(e=>{
+    const i=document.createElement('iframe');
+    i.src=e.href;
+    document.body.appendChild(i);
+    i.onload= _ => {
+        const iw = i.contentWindow;
+        iw.alert=console.log;
+        [...iw.document.querySelectorAll("input[type=radio]")].filter((_,i)=>i%5===0).forEach(x=>x.checked=true);
+        iw.document.querySelector("input#Btn_save").click();
+    }
+})
