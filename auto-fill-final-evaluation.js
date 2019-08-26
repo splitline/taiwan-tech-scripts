0f2807c908d4071136efd7d0c7bd53b532a9849b
黃志仁 <tbsthitw@gmail.com>
2019-08-26 11:00:34 +0800

    Make closure.

--- auto-fill-final-evaluation.js
+++ auto-fill-final-evaluation.js
@@ -1,11 +1,13 @@
-document.querySelectorAll('font>a').forEach(e=>{
-    const i=document.createElement('iframe');
-    i.src=e.href;
-    document.body.appendChild(i);
-    i.onload= _ => {
-        const iw = i.contentWindow;
-        iw.alert=console.log;
-        [...iw.document.querySelectorAll("input[type=radio]")].filter((_,i)=>i%5===0).forEach(x=>x.checked=true);
-        iw.document.querySelector("input#Btn_save").click();
-    }
-})
+(function () {
+    document.querySelectorAll('font>a').forEach(e => {
+        const i = document.createElement('iframe');
+        i.src = e.href;
+        document.body.appendChild(i);
+        i.onload = _ => {
+            const iw = i.contentWindow;
+            iw.alert = console.log;
+            [...iw.document.querySelectorAll("input[type=radio]")].filter((_, i) => i % 5 === 0).forEach(x => x.checked = true);
+            iw.document.querySelector("input#Btn_save").click();
+        }
+    })
+})()
--- gpa-calc.js
+++ gpa-calc.js
@@ -1,5 +1,7 @@
-d = {"A+": 4.3, "A": 4.0, "A-": 3.7, "B+": 3.3, "B": 3.0, "B-": 2.7, "C+": 2.3, "C": 2.0, "C-": 1.7, "D": 1.0, "E": 0.0, "X": 0.0};
-c = [...document.querySelectorAll('#Datagrid4 tr > td:nth-child(4)')].map(e => parseFloat(e.innerText));
-g = ([...document.querySelectorAll('#Datagrid4 tr > td:nth-child(5)')]).map((e, i) => d[e.innerText] * c[i]);
-gpa = g.reduce((a, b) => a + (b || 0), 0) / c.filter((_, i) => !isNaN(g[i])).reduce((a, b) => a + b);
-alert("GPA=" + gpa);
+(function () {
+    d = {"A+": 4.3, "A": 4.0, "A-": 3.7, "B+": 3.3, "B": 3.0, "B-": 2.7, "C+": 2.3, "C": 2.0, "C-": 1.7, "D": 1.0, "E": 0.0, "X": 0.0};
+    c = [...document.querySelectorAll('#Datagrid4 tr > td:nth-child(4)')].map(e => parseFloat(e.innerText));
+    g = ([...document.querySelectorAll('#Datagrid4 tr > td:nth-child(5)')]).map((e, i) => d[e.innerText] * c[i]);
+    gpa = g.reduce((a, b) => a + (b || 0), 0) / c.filter((_, i) => !isNaN(g[i])).reduce((a, b) => a + b);
+    alert("GPA=" + gpa);
+})();
